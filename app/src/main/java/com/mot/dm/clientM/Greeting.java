0956cc6a4a8d27407ea88bbc662198bda057e6f3
Wei Huiping <huiping.wei@motorolasolutions.com>
2016-04-07 21:54:53 +0800

    Add my first Rest client function.

---
+++ app/src/main/java/com/mot/dm/clientM/Greeting.java
@@ -0,0 +1,18 @@
+package com.mot.dm.clientM;
+
+/**
+ * Created by qngv36 on 4/7/2016.
+ */
+public class Greeting {
+    private String id;
+    private String content;
+    public String getId()
+    {
+        return  id;
+    }
+
+    public String getContent()
+    {
+        return  content;
+    }
+}
--- app/src/main/java/com/mot/dm/clientM/LoginActivity.java
+++ app/src/main/java/com/mot/dm/clientM/LoginActivity.java
@@ -2,6 +2,7 @@ package com.mot.dm.clientM;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.mot.dm.R;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,13 +27,44 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                EditText editText = (EditText)findViewById(R.id.user_name);
-                String userName = editText.getText().toString();
-                intent.putExtra("user_name", userName);
-                startActivity(intent);
-                finish();
+                new HttpRequestTask().execute();
             }
         });
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Greeting>
+    {
+        @Override
+        protected Greeting doInBackground(Void...params)
+        {
+            try
+            {
+                String url = "http://rest-service.guides.spring.io/greeting";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+                return greeting;
+            }
+            catch (Exception ex)
+            {
+                Log.e("MainActivity", ex.getMessage(),ex);
+            }
+
+            return  null;
+        }
+
+        protected void onPostExecute(Greeting greeting)
+        {
+            String id = greeting.getId();
+            String content = greeting.getContent();
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            EditText editText = (EditText)findViewById(R.id.user_name);
+            String userName = editText.getText().toString();
+            intent.putExtra("user_name", content);
+            startActivity(intent);
+            finish();
+            Log.d("id", id);
+        }
     }
 }
